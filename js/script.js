ab4800b1b0d62af24ec39178ac0f196dd1e141c3
tduclos <67517520+tduclos@users.noreply.github.com>
2020-06-27 14:44:01 -0400

    Populated test array
    (I also set up the framework for both functions)

--- js/script.js
+++ js/script.js
@@ -10,24 +10,51 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+    {string: 'Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.',
+    source: 'Neil Gaiman',
+    citation: 'Coraline',
+    year: 2002
+    },
+    {string: 'I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.',
+    source: 'Albert Einstein',
+    year:  1929
+    },
+    {string: 'Everything you can imagine is real.',
+    source: 'Pablo Picasso',
+    },
+    {string: 'And, when you want something, all the universe conspires in helping you to achieve it.',
+    source: 'Paulo Coelho',
+    citation: 'The Alchemist',
+    year: 1988
+    },
+    {string: 'Whatever you are, be a good one.',
+    source: 'Abraham Lincoln',
+    }   
+];
 
+console.log(quotes);
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-
+function GetRandomQuote(){
+    
+};
 
 /***
  * `printQuote` function
 ***/
 
-
+function printQuote(){
+       
+};
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
